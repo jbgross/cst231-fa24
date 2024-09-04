18155768706194173db0e081c2c6d149fa1383a4
jbgross <gross.joshua.b@gmail.com>
2024-09-04 15:04:46 -0700

    initial commit from IDEA

---
+++ wk02/Wed.java
@@ -0,0 +1,22 @@
+package wk02;
+
+//import java.util.Scanner;
+//import java.util.Random;
+import java.util.*;
+
+public class /* block comment don't do this */ Wed {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter sides for die: ");
+        int sides = input.nextInt();
+
+        //        int sides = 5;
+//        sides = input.nextInt();
+//        System.out.println("sides = " + sides);
+
+        Random rand = new Random();
+        int roll = (Math.abs(rand.nextInt())%side) + 1;
+        System.out.println("Roll: " + roll);
+
+    }
+}
